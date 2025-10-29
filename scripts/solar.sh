4606568f612b121b01397d94eed481424638366b
zhu hao <1815526897@qq.com>
2025-10-29 18:47:15 +0800

    a

--- scripts/solar.sh
+++ scripts/solar.sh
@@ -72,7 +72,7 @@ do
       --train_epochs 100 \
       --devices 0,1,2,3,4,5,6,7 \
       --use_multi_gpu \
-      --itr 1 --batch_size 32 --learning_rate $learning_rate > logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
+      --itr 1 --batch_size 32 --learning_rate $learning_rate | tee logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
 done
 done
 done
@@ -134,7 +134,7 @@ do
       --train_epochs 100 \
       --devices 0,1,2,3,4,5,6,7 \
       --use_multi_gpu \
-      --itr 1 --batch_size 32 --learning_rate $learning_rate > logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
+      --itr 1 --batch_size 32 --learning_rate $learning_rate | tee logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
 done
 done
 done
@@ -197,7 +197,7 @@ do
       --train_epochs 100 \
       --devices 0,1,2,3,4,5,6,7 \
       --use_multi_gpu \
-      --itr 1 --batch_size 32 --learning_rate $learning_rate > logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
+      --itr 1 --batch_size 32 --learning_rate $learning_rate | tee logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
 done
 done
 done
