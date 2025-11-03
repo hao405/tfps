9ef321eeed8a1c319d72f94771a07637694e167f
zhu hao <1815526897@qq.com>
2025-11-03 10:25:18 +0800

    1

--- scripts/solar.sh
+++ scripts/solar.sh
@@ -23,26 +23,43 @@ export MIOPEN_DEBUG_DISABLE_FIND_DB=1
 export HIP_VISIBLE_DEVICES=$GPU
 # random_seed=2023
 
+# Recommended training config for Solar + PatchTST_MoE_cluster
+BATCH_SIZE=24
+D_MODEL=128
+N_HEADS=4
+E_LAYERS=3
+D_FF=128
+PATCH_LEN=16
+STRIDE=8
+FC_DROPOUT=0.3
+DROPOUT=0.3
+T_NUM_EXPERT=16
+T_TOP_K=1
+F_NUM_EXPERT=16
+F_TOP_K=1
+LR=0.0005
+TRAIN_EPOCHS=100
+MAX_GRAD_NORM=1.0
 
 for seq_len in 96
 do
-for pred_len in 96
+for pred_len in 96 192 336 720
 do
 for random_seed in 2023
 do
-for learning_rate in 0.0005
+for learning_rate in ${LR}
 do
-for T_num_expert in 16
+for T_num_expert in ${T_NUM_EXPERT}
 do
-for T_top_k in 1
+for T_top_k in ${T_TOP_K}
 do
-for F_num_expert in 16
+for F_num_expert in ${F_NUM_EXPERT}
 do
-for F_top_k in 1
+for F_top_k in ${F_TOP_K}
 do
     MIOPEN_DISABLE_CACHE=1 \
     MIOPEN_SYSTEM_DB_PATH="" \
-    HIP_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
+    HIP_VISIBLE_DEVICES="$GPU" \
     python -u ../run_longExp.py \
       --random_seed $random_seed \
       --is_training 1 \
@@ -57,27 +74,31 @@ do
       --pred_len $pred_len \
       --enc_in 137 \
       --c_out 137 \
-      --e_layers 3 \
-      --n_heads 4 \
-      --d_model 16 \
-      --d_ff 64 \
-      --dropout 0.3 \
-      --fc_dropout 0.3 \
+      --e_layers ${E_LAYERS} \
+      --n_heads ${N_HEADS} \
+      --d_model ${D_MODEL} \
+      --d_ff ${D_FF} \
+      --dropout ${DROPOUT} \
+      --fc_dropout ${FC_DROPOUT} \
       --head_dropout 0 \
-      --patch_len 16 \
-      --stride 8 \
+      --patch_len ${PATCH_LEN} \
+      --stride ${STRIDE} \
       --T_num_expert $T_num_expert \
       --T_top_k $T_top_k \
       --F_num_expert $F_num_expert \
       --F_top_k $F_top_k \
       --beta 0.1 \
       --des 'Exp' \
-      --train_epochs 100 \
+      --train_epochs ${TRAIN_EPOCHS} \
       --devices 0,1,2,3,4,5,6,7 \
       --use_multi_gpu \
       --use_gpu True \
-      --gpu 5\
-      --itr 1 --batch_size 32 --learning_rate $learning_rate | tee logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
+      --gpu 0 \
+      --itr 1 --batch_size ${BATCH_SIZE} --learning_rate ${learning_rate} \
+      --max_grad_norm ${MAX_GRAD_NORM} | tee logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
+
+    sleep 2
+
 done
 done
 done
@@ -87,190 +108,4 @@ done
 done
 done
 
-
-for seq_len in 96
-do
-for pred_len in 192
-do
-for random_seed in 2023
-do
-for learning_rate in 0.0005
-do
-for T_num_expert in 4
-do
-for T_top_k in 1
-do
-for F_num_expert in 4
-do
-for F_top_k in 1
-do
-
-    python -u ../run_longExp.py \
-      --random_seed $random_seed \
-      --is_training 1 \
-      --root_path $root_path_name \
-      --data_path $data_path_name \
-      --model_id ${model_id_name}_${seq_len}_${pred_len} \
-      --model $model_name \
-      --data $data_name \
-      --features M \
-      --target 0 \
-      --seq_len $seq_len \
-      --pred_len $pred_len \
-      --enc_in 137 \
-      --c_out 137 \
-      --e_layers 3 \
-      --n_heads 4 \
-      --d_model 16 \
-      --d_ff 64 \
-      --dropout 0.3 \
-      --fc_dropout 0.3 \
-      --head_dropout 0 \
-      --patch_len 16 \
-      --stride 8 \
-      --T_num_expert $T_num_expert \
-      --T_top_k $T_top_k \
-      --F_num_expert $F_num_expert \
-      --F_top_k $F_top_k \
-      --beta 0.1 \
-      --des 'Exp' \
-      --train_epochs 100 \
-      --devices 0,1,2,3,4,5,6,7 \
-      --use_multi_gpu \
-      --use_gpu True \
-      --gpu 5\
-      --itr 1 --batch_size 32 --learning_rate $learning_rate | tee logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
-done
-done
-done
-done
-done
-done
-done
-done
-
-
-for seq_len in 96
-do
-for pred_len in 336
-do
-for random_seed in 2023
-do
-for learning_rate in 0.005
-do
-for T_num_expert in 4
-do
-for T_top_k in 1
-do
-for F_num_expert in 4
-do
-for F_top_k in 1
-do
-
-
-    python -u ../run_longExp.py \
-      --random_seed $random_seed \
-      --is_training 1 \
-      --root_path $root_path_name \
-      --data_path $data_path_name \
-      --model_id ${model_id_name}_${seq_len}_${pred_len} \
-      --model $model_name \
-      --data $data_name \
-      --features M \
-      --target 0 \
-      --seq_len $seq_len \
-      --pred_len $pred_len \
-      --enc_in 137 \
-      --c_out 137 \
-      --e_layers 3 \
-      --n_heads 4 \
-      --d_model 16 \
-      --d_ff 64 \
-      --dropout 0.3 \
-      --fc_dropout 0.3 \
-      --head_dropout 0 \
-      --patch_len 16 \
-      --stride 8 \
-      --T_num_expert $T_num_expert \
-      --T_top_k $T_top_k \
-      --F_num_expert $F_num_expert \
-      --F_top_k $F_top_k \
-      --beta 0.1 \
-      --des 'Exp' \
-      --train_epochs 100 \
-      --devices 0,1,2,3,4,5,6,7 \
-      --use_multi_gpu \
-      --use_gpu True \
-      --gpu 5\
-      --itr 1 --batch_size 32 --learning_rate $learning_rate | tee logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
-done
-done
-done
-done
-done
-done
-done
-done
-
-
-
-for seq_len in 96
-do
-for pred_len in 720
-do
-for random_seed in 2023
-do
-for learning_rate in 0.0005
-do
-for T_num_expert in 8
-do
-for T_top_k in 1
-do
-for F_num_expert in 8
-do
-for F_top_k in 1
-do
-
-    python -u ../run_longExp.py \
-      --random_seed $random_seed \
-      --is_training 1 \
-      --root_path $root_path_name \
-      --data_path $data_path_name \
-      --model_id ${model_id_name}_${seq_len}_${pred_len} \
-      --model $model_name \
-      --data $data_name \
-      --features M \
-      --target 0 \
-      --seq_len $seq_len \
-      --pred_len $pred_len \
-      --enc_in 137 \
-      --c_out 137 \
-      --e_layers 3 \
-      --n_heads 4 \
-      --d_model 16 \
-      --d_ff 64 \
-      --dropout 0.3 \
-      --fc_dropout 0.3 \
-      --head_dropout 0 \
-      --patch_len 16 \
-      --stride 8 \
-      --T_num_expert $T_num_expert \
-      --T_top_k $T_top_k \
-      --F_num_expert $F_num_expert \
-      --F_top_k $F_top_k \
-      --beta 0.1 \
-      --des 'Exp' \
-      --train_epochs 100 \
-      --devices 0,1,2,3,4,5,6,7 \
-      --use_multi_gpu \
-      --use_gpu True \
-      --gpu 5\
-      --itr 1 --batch_size 32 --learning_rate $learning_rate | tee logs/LongForecasting/solar/${model_name}_${model_id_name}_${seq_len}_${pred_len}_${T_num_expert}_${T_top_k}_${F_num_expert}_${F_top_k}_${learning_rate}_0.1.log
-done
-done
-done
-done
-done
-done
-done
-done
+echo "All experiments submitted"
