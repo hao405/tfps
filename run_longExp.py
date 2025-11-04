243ddcdc6733353a6820c70efdc17f2ba238d40c
zhmac <1815526897@qq.com>
2025-11-04 17:11:14 +0800

    a

--- run_longExp.py
+++ run_longExp.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     parser.add_argument('--itr', type=int, default=2, help='experiments times')
     parser.add_argument('--train_epochs', type=int, default=100, help='train epochs')
     parser.add_argument('--batch_size', type=int, default=128, help='batch size of train input data')
-    parser.add_argument('--patience', type=int, default=20, help='early stopping patience')
+    parser.add_argument('--patience', type=int, default=15, help='early stopping patience')
     parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
     parser.add_argument('--des', type=str, default='test', help='exp description')
     parser.add_argument('--loss', type=str, default='mse', help='loss function')
